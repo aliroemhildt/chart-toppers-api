bbfa197bd032549bf0419ab3570538300b43cf00
Ali Roemhildt <aliroemhildt@gmail.com>
2022-03-01 22:23:31 -0700

    Setup server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+// will probably need to do something with cors
+// const cors = require('cors'); but be more specific about what endpoints have access 
+
+app.use(express.json());
+
+app.set('port', 4040);
+
+app.locals.title = 'Billboard Top 5 Songs';
+
